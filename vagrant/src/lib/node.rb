f26f17292ef164b27120f26ab57aa94d205ea82b
Juan Pardinas Barbara <jpardinas23@gmail.com>
2023-12-10 14:20:54 +0100

    Implement creation of workers #19

--- vagrant/src/lib/node.rb
+++ vagrant/src/lib/node.rb
@@ -110,6 +110,97 @@ module Node
     
     
   end
+
+  def self.create_workers(config)
+    (1..CONSTANTS::CLUSTER_NODES_WORKERS_COUNT).each do |i|
+    
+      config.vm.define vm_name = (i < 10 ? CONSTANTS::VBOX_NETWORK_HOSTNAME_PREFIX + "worker-node0#{i}" : CONSTANTS::VBOX_NETWORK_HOSTNAME_PREFIX + "worker-node#{i}") do |node|
+        $logger.debug(self, "Creating control server #{vm_name}...")
+        node.vm.boot_timeout = CONSTANTS::VAGRANT_BOOT_TIMEOUT
+
+        node.vm.box = CONSTANTS::VAGRANT_BOX_NAME
+        node.vm.box_version = CONSTANTS::VAGRANT_BOX_VERSION
+        node.vm.box_check_update = true
+
+        node.vm.hostname = vm_name
+
+        $logger.debug(self, "Setting up the network...")
+        ip_sections = CONSTANTS::VBOX_NETWORK_PRIVATE_IP.match(/^([0-9.]+\.)([^.]+)$/)
+        ip_nw = ip_sections.captures[0]
+        ip_start = Integer(ip_sections.captures[1])
+        ip = ip_nw + (ip_start + i).to_s
+        $logger.key_value(self, "Control server network", "default", Logger::DEBUG)
+        node.vm.network "private_network", ip: ip
+        $logger.debug(self, "Network setted up.")
+
+        node.vm.provider :virtualbox do |vb|
+          vb.name = vm_name
+          vb.memory = CONSTANTS::CLUSTER_NODES_WORKERS_MEMORY
+          vb.cpus = CONSTANTS::CLUSTER_NODES_WORKERS_CPU
+          vb.customize ["modifyvm", :id, "--groups", ("/" + CONSTANTS::VBOX_VM_GROUP_NAME)]
+        end
+
+        $logger.debug(self, "Adding the keyboard layout shell provisioner...")
+        Utils.define_shell_provision node, "keyboard layout", path_to_script: CONSTANTS::SCRIPT_KEYBOARD, privileged: false
+
+        $logger.debug(self, "Adding the apt update shell provisioner...")
+        Utils.define_shell_provision node, "apt update", script_content: "apt update -y", privileged: true
+
+        $logger.debug(self, "Adding the python3 installation shell provisioner...")
+        Utils.define_shell_provision node, "python3 installation", path_to_script: CONSTANTS::SCRIPT_INSTALL_PYTHON3, privileged: false
+
+        $logger.debug(self, "Adding the ansible-core installation shell provisioner...")
+        Utils.define_shell_provision node, "ansible-core installation", script_content: "pip3 install --user ansible-core==#{CONSTANTS::ANSIBLE_CORE_VERSION}", privileged: false
+
+        $logger.debug(self, "Adding the ansible folder sync...")
+        node.vm.synced_folder CONSTANTS::ANSIBLE_FOLDER_HOST, CONSTANTS::ANSIBLE_FOLDER_TARGET, owner: "vagrant", group: "vagrant", mount_options: ["dmode=775,fmode=664"]
+
+        $logger.debug(self, "Adding the clean inventory/local shell provisioner...")
+        Utils.define_shell_provision node, "clean inventory/local", script_content: "rm -rf #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/inventory/local && touch #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/inventory/local", privileged: false
+        
+        $logger.debug(self, "Adding the inventory/local shell provisioner...")
+        Utils.define_shell_provision node, "inventory/local", script_content: "echo '[local]\n#{vm_name} ansible_connection=local' >> #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/inventory/local", privileged: false
+
+        $logger.debug(self, "Adding the cp k8s-requirements shell provisioner...")
+        Utils.define_shell_provision node, "ansible cp k8s-requirements", script_content: "cp #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/requirements/k8s-requirements-template.yml #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/requirements/k8s-requirements.yml", privileged: false
+        
+        $logger.debug(self, "Adding the replace k8s-requirements shell provisioner...")
+        Utils.define_shell_provision node, "ansible replace k8s-requirements", script_content: "find #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/requirements/k8s-requirements.yml -type f -exec sed -i 's|REPLACE_LOCATION|#{CONSTANTS::ANSIBLE_FOLDER_TARGET}|g' {} +", privileged: false
+
+        $logger.debug(self, "Adding the cp playbook init shell provisioner...")
+        Utils.define_shell_provision node, "ansible cp playbook init", script_content: "cp #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/playbooks/init-worker-template.yml #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/playbooks/init-worker.yml", privileged: false
+
+        $logger.debug(self, "Adding the replace playbook init shell provisioner...")
+        Utils.define_shell_provision node, "ansible replace playbook init", script_content: "find #{CONSTANTS::ANSIBLE_FOLDER_TARGET}/playbooks/init-worker.yml -type f -exec sed -i 's|REPLACE_WORKER_HOST|#{vm_name}|g' {} +", privileged: false
+
+        $logger.debug(self, "Adding the ansible_local provisioner...")
+        node.vm.provision "ansible_local" do |ansible_local| # https://developer.hashicorp.com/vagrant/docs/provisioning/ansible_common
+          ansible_local.install = false
+          ansible_local.compatibility_mode = "2.0"
+          ansible_local.verbose = true
+          ansible_local.provisioning_path = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}"
+          ansible_local.playbook = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}/playbooks/init-worker.yml"
+          ansible_local.vault_password_file = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}/.vault_password"
+          ansible_local.inventory_path = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}/inventory/local"
+          ansible_local.config_file = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}/ansible.cfg"
+          ansible_local.galaxy_role_file = "#{CONSTANTS::ANSIBLE_FOLDER_TARGET}/requirements/k8s-requirements.yml"
+          # ansible_local.galaxy_roles_path = "/home/vagrant/.ansible/collections/" TODO: verify if we can delete it
+        end
+        
+        $logger.key_value(self, "IP", ip, Logger::INFO)
+        $logger.success(self, "Control server #{vm_name} created.")
+        
+        @@control_server_data = {
+          "ip" => ip,
+          "vm_name" => vm_name
+        }
+        _log_nodes_info()
+      end
+
+    end
+    
+    
+  end
   
   
   def self._log_nodes_info
